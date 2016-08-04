8fc66b65cfe81ef3d8f6000b1db9bd2a7b10270c
rkushner <rkushnereit@usgs.gov>
2016-08-04 11:15:47 -0500

    Added ulmo to bring in USGS stream gauge data faster. The catch is you have to update the code with the nwis.hdf5.update_sitedata() I do not know if it has to be done every year, but it has to be done for every site.

--- Basins.py
+++ Basins.py
@@ -2,27 +2,46 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import style
 import time
+from ulmo.usgs import nwis
+import numpy as np
+
 start_time = time.time()
 style.use(['fivethirtyeight','ggplot']) #,'dark_background'
 
 def master(Basin_name = 'Nueces', year=2015, ET_input = 0, Separation_Method = 'IOH',k=.7531,C=.2469,gamma=0, basin_csv='#',USGS_site_ID = ('08192000')):
-
     basin_df = pd.read_csv('Gauge_to_basin_table.csv')
     basin_df = basin_df.set_index(basin_df['Basin'])
-    print(basin_df)
 
 
     site_id = basin_df.loc[Basin_name]['Lower_Gauge_ID']
     site_id = str(site_id.replace('"', ''))
-    USGS_site_ID = site_id
+    USGS_site_ID = str(site_id)
+    print(USGS_site_ID)
+    #discharge_data = pd.read_table(('http://waterdata.usgs.gov/nwis/dv?cb_00060=on&format=rdb&site_no=' + str(USGS_site_ID) + '&referred_module=sw&period=&begin_date=' + str(year) + '-01-01&end_date=' + str(year) + '-12-31'), skiprows=25)
+    #nwis.hdf5.update_site_data(USGS_site_ID)
+    data = nwis.hdf5.get_site_data(USGS_site_ID, parameter_code='00060:00003')['00060:00003']
+    discharge_data = pd.DataFrame(data['values']).drop(['last_checked', 'last_modified', 'qualifiers'], axis=1).set_index('datetime')
+    discharge_data.value = discharge_data.value.apply(np.float)
+    discharge_data.index = pd.to_datetime(discharge_data.index).to_period('D')
 
-    discharge_data = pd.read_table(('http://waterdata.usgs.gov/nwis/dv?cb_00060=on&format=rdb&site_no=' + str(USGS_site_ID) + '&referred_module=sw&period=&begin_date=' + str(year) + '-01-01&end_date=' + str(year) + '-12-31'), skiprows=25)
-    discharge_data = discharge_data[discharge_data['agency_cd'] != '5s']
-    #discharge_data = discharge_data.drop([0])
-    #C = 1-k
-    discharge_data.reset_index(drop = True,inplace = True)
-    print(discharge_data.head())
-    print(discharge_data.tail())
+    discharge_data[discharge_data.values == -999999] = np.nan
+    daily_groups = discharge_data.groupby((lambda d: d.month, lambda d: d.day))
+    means = daily_groups.mean()
+
+    start = str(str(year)+('0101'))
+    end = str(str(year)+('1231'))
+
+    discharge_data = discharge_data.ix[start:end].copy()
+    discharge_data['Discharge'] = discharge_data['value']
+    del discharge_data['value'] #.copy()
+    # discharge_data.reset_index(drop = True,inplace = True)
+    # print(discharge_data.head(10))
+    # discharge_data = discharge_data[discharge_data['agency_cd'] != '5s']
+    # #discharge_data = discharge_data.drop([0])
+    # #C = 1-k
+    # discharge_data.reset_index(drop = True,inplace = True)
+    # print(discharge_data.head())
+    # print(discharge_data.tail())
 
     basin_csv = basin_df.loc[Basin_name]['Lower_Basin_ID']
     basin_csv = str(basin_csv.replace('"', '') + '.csv')
@@ -65,21 +84,20 @@ def master(Basin_name = 'Nueces', year=2015, ET_input = 0, Separation_Method = '
     yearly_total_basin_csv = Monthly['monthly_weighted_average'].sum()
     area = sum_Area_basin_csv
     Precip = yearly_total_basin_csv
-    print('The square footage of this basin is ' + str(sum_Area_basin_csv) + ' ft^2')
-    print('The Precipitation for the year '+str(year) +' is ' +str(yearly_total_basin_csv) + ' inches.')
+    print('The square footage of this basin is ' + str(sum_Area_basin_csv) + ' ft^2 or ' + str(sum_Area_basin_csv*(2.26598**(-5))) + ' acres or ' + str(sum_Area_basin_csv*(3.587**(-8))))
+    print('The Precipitation for the year '+str(year) +' is ' + str(yearly_total_basin_csv) + ' inches.')
 
-    discharge_df = pd.DataFrame()
-    discharge_df = discharge_df.reset_index(drop=True)
-    discharge_df['Date'] = discharge_data[[2]]
-    discharge_df['Discharge'] = discharge_data[[3]].astype(float)
-    discharge_df = discharge_df.set_index(pd.DatetimeIndex(discharge_df['Date']))
+    # discharge_df = pd.DataFrame()
+    # discharge_df = discharge_df.reset_index(drop=True)
+    # print(discharge_df.head())
+    discharge_df = discharge_data
+    discharge_df.reset_index(drop = False, inplace = True)
+    #discharge_df['Discharge'] = discharge_data[['value']].astype(float)
+    #discharge_df = discharge_df.set_index(pd.DatetimeIndex(discharge_df['datetime']))
     discharge_df['baseflow'] = 0.0
-    print(discharge_df.head())
-    print(discharge_df.dtypes)
+
 
     if Separation_Method == 'IOH':
-        i=363
-        print(float(discharge_df.iloc[i]['Discharge']) * k)
         for i in range(0, len(discharge_df['Discharge'])-1):
             if (discharge_df.iloc[i]['Discharge'] * k <= (discharge_df.iloc[i - 1]['Discharge'])) & (discharge_df.iloc[i]['Discharge'] * k <= discharge_df.iloc[i + 1]['Discharge']):
                 discharge_df.ix[i,['baseflow']] = discharge_df.iloc[i]['Discharge']
@@ -94,7 +112,6 @@ def master(Basin_name = 'Nueces', year=2015, ET_input = 0, Separation_Method = '
     #             discharge_df.ix[i,['baseflow']] = None
     #     for i in range(0, len(discharge_df['Discharge'])-1):
     #         if (discharge_df.loc[i]['Discharge'] * (k**i) <= discharge_df.iloc[i - 1]['Discharge']) & (discharge_df.iloc[i]['Discharge'] * k <= discharge_df.iloc[i + 1]['Discharge']):
-
 
     elif Separation_Method == 'Chapman':
         for i in range(0, len(discharge_df['Discharge'])-1):
@@ -139,16 +156,18 @@ def master(Basin_name = 'Nueces', year=2015, ET_input = 0, Separation_Method = '
             discharge_df.ix[i,['baseflow']] = discharge_df.iloc[i]['Discharge']
 
 
-    del discharge_df['Date']
-    BFI = (discharge_df['baseflow'].sum())/(discharge_df['baseflow'].sum() + discharge_df['Discharge'].sum())
-    print(BFI)
+    del discharge_df['datetime']
+    BFI = (discharge_df['baseflow'].sum())/discharge_df['Discharge'].sum()
+    if BFI >= .61:
+        BFI = (discharge_df['baseflow'].sum())/(discharge_df['baseflow'].sum() + discharge_df['Discharge'].sum())
+    print('BFI = ' + str(BFI))
     recharge = (BFI * ((Precip - ET_input)/12) * area)
     recharge = recharge * (2.29568*(10**(-5))) # convert to acre-ft
     print('The estimated recharge using the ' + str(Separation_Method) + ' method, and a k value of ' + str(k) + ' is ' , recharge , ' acre-ft')
     discharge_df['Fld_Flw'] = None
     print("--- %s seconds ---" % (time.time() - start_time))
-    ax = discharge_df.plot(title= basin_df.loc[Basin_name]['Lower_Guage_name'] + ', k = ' + str(k), figsize=(15, 10), legend=True, fontsize=12)
-    ax.set_xlabel('Date', fontsize=12)
+    ax = discharge_df.plot(title= basin_df.loc[Basin_name]['Lower_Gauge_name'] + ', k = ' + str(k), figsize=(15, 10), legend=True, fontsize=12)
+    ax.set_xlabel('datetime', fontsize=12)
     ax.set_ylabel('discharge_cfs', fontsize=12)
     plt.show()
 
--- basin_test.py
+++ basin_test.py
@@ -1,8 +1,9 @@
 import Basins
 import pandas as pd
+from ulmo.usgs import nwis
+import numpy as np
 
-
-Basins.master(Basin_name= 'Nueces',year=2015, ET_input = 0, Separation_Method='IOH',k=.87517531,C=.249,gamma=00)
+Basins.master(Basin_name= 'Frio',year=2015, ET_input = 0, Separation_Method='IOH',k=.87517531,C=.249,gamma=00)
 
 #USGS 08169000 Comal Rv at New Braunfels, TX
 
